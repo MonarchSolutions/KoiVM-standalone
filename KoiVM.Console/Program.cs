fd9147e38aef10dd1877ab37e40ddbaa29f561e4
Modify24x7 <goravgupta776@gmail.com>
2019-12-28 18:59:25 +0530

    Initial commit

---
+++ KoiVM.Console/Program.cs
@@ -0,0 +1,43 @@
+﻿using dnlib.DotNet;
+using KVM;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace KoiVM.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                ExceuteKoi(@args[0], @args[1], "", null);
+            }
+            if (args.Length == 3)
+            {
+                ExceuteKoi(@args[0], @args[1], args[2], null);
+            }
+            if (args.Length == 4)
+            {
+                ExceuteKoi(@args[0], @args[1], args[2], args[3]);
+            }
+
+            Process.GetCurrentProcess().Kill();
+        }
+
+       private static void ExceuteKoi(String input, string outPath, string snPath, string snPass)
+        {
+            try
+            {
+                new KVMTask().Exceute(ModuleDefMD.Load(input), outPath, snPath, snPass);
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# KoiVM
+KoiVM No ConfuserEx
+
+### Usage
+    KoiVM.Console.exe <input> <output>
+    KoiVM.Console.exe <input> <output> <pfx or snk>
+    KoiVM.Console.exe <input> <output> <pfx or snk> <key password>
+	
+    KoiVM.Console.exe test.exe test.koivm.exe
+	KoiVM.Console.exe test.exe test.koivm.exe test.PFX
+	KoiVM.Console.exe test.exe test.koivm.exe test.PFX testPass
+	
+### License
+    dnlib: https://github.com/0xd4d/dnlib/blob/master/LICENSE.txt
+    KoiVM: https://github.com/yck1509/KoiVM/blob/master/README.md
